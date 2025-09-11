3d8eb712576bbe5dd80e09c571a5a101f586c6fe
Franklin <franklin.s@tech-swing.com>
2025-09-11 14:54:51 +0530

    Search icon changed

--- src/app/(main)/user-role-management/roles/RolePage.tsx
+++ src/app/(main)/user-role-management/roles/RolePage.tsx
@@ -1,5 +1,212 @@
-export default function RolePage(){
-    return(
-        <>Role page</>
-    );
+"use client";
+import AgGrid1 from "@/components/shared-ui/AgGrid1";
+import CardComponent from "@/components/shared-ui/CardAgGrid";
+import { Box, Grid, IconButton } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useState } from "react";
+import type { ICellRendererParams } from 'ag-grid-community';
+import MuiDialog from "@/components/shared-ui/MuiDialog";
+import { Formik } from "formik";
+import FormikController from "@/components/mui-components/formik-controller/FormikController";
+import MuiButton from "@/components/mui-components/button/MuiButton";
+
+const ActionRenderer = (params: ICellRendererParams & { onEdit: (data: any) => void; onDelete: (data: any) => void }) => {
+  const { data, onEdit, onDelete } = params;
+  return (
+    <Box className="flex justify-center justiffy-around">
+      <IconButton onClick={() => onEdit(data)}>
+        <EditIcon sx={{ fontSize: '18px', color: "var(--color-primary)" }} />
+      </IconButton>
+      <IconButton onClick={() => onDelete(data)} >
+        <DeleteIcon sx={{ fontSize: '18px', color: "var(--color-primary)" }} />
+      </IconButton>
+    </Box>
+  );
+};
+const ViewRenderer = ( ) => {
+  // const { data, onEdit, onDelete } = params;
+  return (
+    <Box >
+      {/* <button>View</button> */}
+      <MuiButton type="grid-btn">View</MuiButton>
+    </Box>
+  );
+};
+
+export default function UserPage() {
+  const [pageControl, setPageControl] = useState({
+    isEdit: false,
+    isOpen: false,
+  });
+  const [rowData, setRowData] = useState([
+    ]);
+
+  const handleEdit = (row: any) => {
+    setPageControl((prev) => ({ ...prev, isEdit: true, isOpen: true }));
+  };
+  const handleDelete = (row: any) => {
+    const updatedRow = rowData.filter((item: any) => item.userId !== row.userId);
+    setRowData(updatedRow);
+  };
+  let columnDefs = [
+    {
+      field: 'userId',
+      headerName: 'ID',
+       cellStyle: { textAlign: 'left' },
+    },
+    {
+      field: 'loginUsername',
+      headerName: 'FullName',
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      // minWidth: 180,
+
+    },
+    {
+      field: 'roleName',
+      headerName: 'Role Name',
+    },
+    {
+      field: 'userStatusId',
+      headerName: 'Status',
+    },
+    {
+      field: 'createdDatetime',
+      headerName: 'Created DateTime',
+    },
+    {
+      field: 'lastUpdatedDatetime',
+      headerName: 'Modified DateTime',
+    },
+    {
+      field: 'action',
+      headerName: 'Actions',
+      filter: false,
+      resizable: false,
+      editable: false,
+      sortable: false,
+      cellRenderer: ActionRenderer,
+      cellStyle: { display: 'flex', justifyContent: 'center', alignItems: 'center'},
+      cellRendererParams: {
+        onEdit: handleEdit,
+        onDelete: handleDelete,
+      },
+    },
+    {
+      field: 'button',
+      headerName: 'User Detail',
+      filter: false,
+      resizable: false,
+      editable: false,
+      sortable: false,
+      cellStyle: { display: 'flex', justifyContent: 'center', alignItems: 'center'},
+      cellRenderer: ViewRenderer,
+    },
+  ];
+
+  const handleAdd = () => {
+    setPageControl((prev) => ({ ...prev, isEdit: false, isOpen: true }));
+  }
+  const initialValues = {
+
+  }
+  const handleSubmit = () => {
+
+  }
+  return (
+    <Box>
+      <CardComponent>
+        <AgGrid1 rowData={rowData} columnDefs={columnDefs} 
+        actionButton={{
+            handleActionButton: handleAdd,
+            label:'Add'
+          }}
+          // leftActionButton={
+          // <MuiButton type="basic-btn" onClick={handleAdd}>Add</MuiButton>}
+        />
+      </CardComponent>
+      {pageControl.isOpen && (
+        <>
+          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            <MuiDialog
+              open={pageControl.isOpen}
+              onClose={() => setPageControl(prev => ({ ...prev, isOpen: false }))}
+              width="700px"
+              dialogTitle={pageControl.isEdit ? "Edit User" : "Add User"}
+              >   
+              <Grid container spacing={3} justifyContent="center">
+                <Grid size={{ xs: 6 }}>
+                  <FormikController
+                    control="textField"
+                    label="User Name"
+                    name="userName"
+                    required
+                  />
+                </Grid>
+                <Grid size={{ xs: 6 }}>
+                  <FormikController
+                    control="textField"
+                    label="User Name"
+                    name="userName"
+                    required
+                  />
+                </Grid>
+                <Grid size={{ xs: 6 }}>
+                  <FormikController
+                    control="textField"
+                    label="User Name"
+                    name="userName"
+                    required
+                  />
+                </Grid>
+                <Grid size={{ xs: 6 }}>
+                  <FormikController
+                    control="textField"
+                    label="User Name"
+                    name="userName"
+                    required
+                  />
+                </Grid>
+                <Grid size={{ xs: 6 }}>
+                  <FormikController
+                    control="textField"
+                    label="User Name"
+                    name="userName"
+                    required
+                  />
+                </Grid>
+                <Grid size={{ xs: 6 }}>
+                  <FormikController
+                    control="textField"
+                    label="User Name"
+                    name="userName"
+                    required
+                  />
+                </Grid>
+                <Grid size={{ xs: 6 }}>
+                  <FormikController
+                    control="textField"
+                    label="User Name"
+                    name="userName"
+                    required
+                  />
+                </Grid>
+                <Grid size={{ xs: 6 }}>
+                  <FormikController
+                    control="textField"
+                    label="User Name"
+                    name="userName"
+                    required
+                  />
+                </Grid>
+              </Grid>
+            </MuiDialog>
+          </Formik>
+        </>
+      )}
+    </Box>
+  );
 }
--- src/components/shared-ui/AgGrid1.tsx
+++ src/components/shared-ui/AgGrid1.tsx
@@ -4,16 +4,19 @@ import type { GridApi, Column, FirstDataRenderedEvent, ColDef, PaginationChanged
 import { useCallback, useMemo, useRef } from 'react';
 import { Search } from 'lucide-react';
 import { agGridTheme } from '@/lib/agGridTheme';
-import { Box } from '@mui/material';
+import { Box, Grid, InputAdornment } from '@mui/material';
 import MuiButton from '../mui-components/button/MuiButton';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import SearchIcon from '@mui/icons-material/Search';
+import FormikController from '@/components/mui-components/formik-controller/FormikController';
+import { Formik } from 'formik';
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 const AgGrid1: React.FC<any> = (props) => {
   const { actionButton } = props;
-  const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
-  const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+  const containerStyle = useMemo(() => ({ width: '100%', height: 'auto' }), []);
+  const gridStyle = useMemo(() => ({ height: 'auto', width: '100%' }), []);
   const defaultColDef = {
     // filter: true,
     resizable: true,
@@ -33,7 +36,6 @@ const AgGrid1: React.FC<any> = (props) => {
       };
     },
   };
-
   const gridRef = useRef<AgGridReact>(null);
   const columnApiRef = useRef<Column | null>(null);
 
@@ -45,6 +47,11 @@ const AgGrid1: React.FC<any> = (props) => {
 
   const onFilterTextChanged = useCallback(() => {
     gridRef.current!.api.setGridOption('quickFilterText', (document.getElementById('filter-text-box') as HTMLInputElement).value);
+    gridRef.current!.api.resetRowHeights();
+  }, []);
+
+  const onFilterChanged = useCallback((params: any) => {
+    params.api.resetRowHeights();
   }, []);
 
   const autoSizeColumns = useCallback(({ api, columnApi }: { api: GridApi; columnApi: any }) => {
@@ -89,27 +96,59 @@ const AgGrid1: React.FC<any> = (props) => {
     [autoSizeColumns]
   );
   const autoSizeStrategy = useMemo(() => {
-          return {
-              type: "fitCellContents",
-              defaultMaxWidth: 800,
-              defaultMinWidth: 100,
-          };
-      }, []);
+    return {
+      type: "fitCellContents",
+      defaultMaxWidth: 800,
+      defaultMinWidth: 100,
+    };
+  }, []);
   return (
     <Box style={containerStyle}>
-      <Box mb={2} className="flex items-center gap-2">
+
+      {/* <Box mb={2} className="flex items-center gap-2">
         {actionButton && actionButton?.label && (
           <MuiButton type="basic-btn" className={actionButton.className} onClick={actionButton?.handleActionButton}>
-            <GroupAddIcon sx={{ fontSize: "20px",paddingRight:"4px"}}/>
+            <GroupAddIcon sx={{ fontSize: "20px", paddingRight: "4px" }} />
             {actionButton?.label}
           </MuiButton>
         )}
         <Box px={1} py={0.5} className="flex items-center gap-2 border border-[var(--color-secondary)] rounded-md  bg-[var(--color-accent)] focus-within:ring-1 focus-within:ring-[var(--color-secondary)]">
           <Search size={16} className="text-[var(--color-secondary)]" />
           <input id="filter-text-box" type="text" placeholder="Search" onInput={onFilterTextChanged} className="text-sm focus:outline-none w-40" />
-        </Box>
-        
-      </Box>
+        </Box> 
+      </Box> */}
+
+    <Formik initialValues={{search:""}} onSubmit={()=>{}}>
+      <Grid container justifyContent="flex-start" alignItems="center" >
+        <Grid size={{ xs: 1 }}>
+          {actionButton && actionButton?.label && (
+            <MuiButton type="basic-btn" className={actionButton.className} onClick={actionButton?.handleActionButton}>
+              <GroupAddIcon sx={{ fontSize: "20px", paddingRight: "4px" }} />
+              {actionButton?.label}
+            </MuiButton>
+          )}
+        </Grid>
+         <Grid size={{ xs: 3 }} mb={2}>
+          <FormikController
+                  id="filter-text-box"
+                  control="textField"
+                  label="Search"
+                  name="search"
+                  onInput={onFilterTextChanged}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                         <SearchIcon sx={{ marginBottom:"7px",fontSize:"19px",color:"var(--color-secondary)"}}/>
+                        {/* <Search size={17} className="text-[var(--color-secondary)] m-10 p-10"/> */}
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+         </Grid>
+      </Grid>
+      </Formik>
+
+
       <Box className="rounded-t-lg overflow-hidden" style={gridStyle}>
         <AgGridReact
           ref={gridRef}
@@ -122,6 +161,8 @@ const AgGrid1: React.FC<any> = (props) => {
           theme={agGridTheme}
           rowHeight={35}
           headerHeight={40}
+          onFilterChanged={onFilterChanged}
+          //  domLayout={hasRow ? "autoHeight": "normal"}
           domLayout="autoHeight"
           overlayNoRowsTemplate="No Rows To Show"
           suppressDragLeaveHidesColumns
